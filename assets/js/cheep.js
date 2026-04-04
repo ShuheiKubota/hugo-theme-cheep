88ab8b262c697cf6ae72c7f986c7815e58bd095b
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-04-04 21:55:55 +0900

    feat: .may_be_outdated
    config.toml
    [params]
      outdateUnit = "year"
      outdateFreshLimit = 1

--- assets/js/cheep.js
+++ assets/js/cheep.js
@@ -1,32 +1,65 @@
 function replaceSince(spanElem) {
-    var v = spanElem.firstChild.data;
-    spanElem.title = v;
-    
-    var now = new Date(Date.now());
-    var mod = new Date(v);
+    var v = spanElem.firstChild.data
+    var mod = new Date(v)
 
-    var d = now.getTime() - mod.getTime();
-    if (d < 60 * 60 * 1000) {
-        spanElem.firstChild.data = Math.floor(d / (60 * 1000)) + " minutes ago";
-        return
-    } else if (d < 24 * 60 * 60 * 1000) {
-        spanElem.firstChild.data = Math.floor(d / (60 * 60 * 1000)) + " hours ago";
-        return
-    } else if (d < 28 * 24 * 60 * 60 * 1000) {
-        spanElem.firstChild.data = Math.floor(d / (24 * 60 * 60 * 1000)) + " days ago";
-        return
+    var s = since(new Date(Date.now()), new Date(v))
+    console.log(s)
+
+    var expr = "now"
+    if (s.years) {
+        expr = s.years + " years ago"
+    } else if (s.months) {
+        expr = s.months + " months ago"
+    } else if (s.days) {
+        expr = s.days + " days ago"
+    } else if (s.hours) {
+        expr = s.hours + " hours ago"
+    } else if (s.minutes) {
+        expr = s.minutes + " minutes ago"
     }
 
-    var dym = (now.getFullYear() * 12 + now.getMonth() + 1) - (mod.getFullYear() * 12 + mod.getMonth() + 1);
-    if (dym < 12)  {
-        spanElem.firstChild.data = dym + " months ago";
-        return
-    } else if (dym > 0) {
-        spanElem.firstChild.data = Math.trunc(dym / 12) + " years ago";
-        return
+    spanElem.title = v
+    spanElem.firstChild.data = expr
+}
+
+// 2 year, 35 months
+// 11 months, 360 days
+// 27 days
+// 23 hours 59 minuts
+function since(now, dt) {
+    var s = null
+    var d = now.getTime() - dt.getTime()
+
+    var dym = (now.getFullYear() * 12 + now.getMonth() + 1) - (dt.getFullYear() * 12 + dt.getMonth() + 1);
+    if (dym >= 12)  {
+        s = {...(s||{}), years: Math.trunc(dym / 12)}
+    }
+    if (dym > 0) {
+        s = {...(s||{}), months: dym}
+    }
+    if (s === null || s.years === undefined) {
+        var days = Math.floor(d / (24 * 60 * 60 * 1000))
+        if (days > 0) {
+            s = {...(s||{}), days: Math.floor(d / (24 * 60 * 60 * 1000))}
+        }
+    }
+    if (s !== null) {
+        return s
     }
 
-    spanElem.firstChild.data = "now";
+    d %= 24 * 60 * 60 * 1000
+    console.log('d', d)
+
+    if (d >= 60 * 60 * 1000) {
+        s = {...(s||{}), hours: Math.floor(d / (60 * 60 * 1000))}
+        d %= 60 * 60 * 1000
+    }
+    if (d >= 60 * 1000) {
+        //return {minutes: Math.floor(d / (60 * 1000))}
+        s = {...(s||{}), minutes: Math.floor(d / (60 * 1000))}
+    }
+
+    return s || {}
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -40,5 +73,36 @@ document.addEventListener('DOMContentLoaded', function() {
         btn.setAttribute('class', 'copy')
         btn.addEventListener('click', (be) => navigator.clipboard.writeText(text))
         e.appendChild(btn)
+    })
+
+    document.querySelectorAll('.may_be_outdated').forEach((e) => {
+        var mod = new Date(e.getAttribute('cheep-since'))
+        var limitStr = e.getAttribute('cheep-limit')
+        var limit = 0
+        try {
+            limit = parseInt(limitStr)
+        } catch (e) {
+            console.log(e)
+            return
+        }
+        var limitUnit = e.getAttribute('cheep-limit-unit')
+
+        if ((limit === null) || (limitUnit === null)) {
+            return
+        }
+
+        var s = since(new Date(Date.now()), mod)
+
+        var over = ''
+        if (limitUnit === 'year' && s.years !== undefined && limit < s.years) {
+            over = s.years + '年'
+        } else if (limitUnit === 'month' && s.months !== undefined && limit < s.months) {
+            over = s.months + 'ヵ月'
+        }
+        if (over !== '') {
+            e.innerHTML = '最終更新日から ' + over + ' 以上が経過しています。<br/>内容が古くなっている可能性があるのでご注意ください。'
+        }
+
+        console.log('debug' , 'data', e.innerHTML)
     })
 })
